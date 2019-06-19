54e9cd1042c504d3891bcc4bc076fc2aaa01c128
Munch-Z <46868472+Munch-Z@users.noreply.github.com>
2019-06-19 15:01:08 -0700

    Questions Object setup, and HTML variables declared.

---
+++ assets/javascript/game.js
@@ -0,0 +1,118 @@
+//screens to hide and show
+
+let startScreen = document.getElementById('startScreen');
+let gameScreen = document.getElementById('gameScreen');
+let finalScreen = document.getElementById('finalScreen');
+let transitionScreen = document.getElementById('transitionScreen');
+
+//buttons
+
+let playGameBtn = document.getElementById('playGame');
+let submitBtn = document.getElementById('submitAnswer');
+let restartBtn = document.getElementById('restartGame');
+
+//text locations
+
+let questionDisplay = document.getElementById('questionHeader');
+let answersDiv = document.getElementById('answerSelection');
+let rightAnswer = document.getElementById('rightAnswers');
+let wrongAnswer = document.getElementById('wrongAnswers');
+let timerDisplay = document.getElementById('timer');
+let transitionText = document.getElementById('transitionHeader');
+let correctAnswerHTML = document.getElementById('correctAnswer');
+
+//global stats
+let losses = 0;
+let wins = 0;
+let timer = 15;
+
+//Counts questions to reset at max
+let count = 0;
+
+//questions and answers
+
+let questionsObj = [{
+    question : "What has been shown to be the most secure type of password for devices and accounts?",
+    answers: ["A very long list of random words combined with symbols", "Your pets name", "Your mothers maiden name", "The model of your first car"],
+    correct: "A very long list of random words combined with symbols"
+},
+{
+    question : " What are the words of the acronym CD-ROM?",
+    answers: ["Compact Disc Read Only Memory", "Compact Disc Runs On Machine", "Compact Disc Relinquish Owner Machine", "Car Driver Really Oughta Move"],
+    correct: "Compact Disc Read Only Memory"
+},
+{
+    question : " One of the first electronic computers, located in Philadelphia, occupied 167 square metres, weighed 27 tons and consumed 150kW of electricity. What was it called?",
+    answers: ["ENIAC", "Big Blue", "Large and in charge", "Computer McComputer Face"],
+    correct: "ENIAC"
+},
+{
+    question : "Which British mathematician and inventor, known as the 'Father of the Computer', designed a mechanical computer called the Analytical Engine which was an early forerunner of the computer we know today?",
+    answers: ["Charles Babbage", "Daniel Craig", "Benedict Cumberbatch", "Bill Gates"],
+    correct: "Charles Babbage"
+},
+{
+    question : "When IBM chose the name System/360 (or S/360), it was selected to be all-encompassing (a compass rose was the logo for the S/360). As such the System was designed to handle all EXCEPT which of the following types of processing?",
+    answers: ["Graphics Processing", "Batch Processing", "Scientific Processing", "Communications Processing"],
+    correct: "Graphics Processing"
+},
+{
+    question : "Which general term refers to all kinds of harmful software, including viruses, worms, trojan horses, and spyware?",
+    answers: ["Malware", "Firmware", "Meanware", "Badware"],
+    correct: "Malware"
+},
+{
+    question : "The CPU controls all of a computer's functions. It acts as the brain of the computer. What does CPU stand for?",
+    answers: ["Central Processing Unit", "Central Python Unit", "Cool Part Unix", "Computer Processing Unit"],
+    correct: "Central Processing Unit"
+},
+{
+    question : "Which of the following is a non-impact printer?",
+    answers: ["ink jet printer", "dot matrix printer", "daisy-wheel printer", "line printer"],
+    correct: "ink jet printer"
+},
+{
+    question : "The 'Caesar Cipher' is also known as what?",
+    answers: ["Rot3", "Et tu, Brute? Cipher", "Backstabber", "Salad Cipher"],
+    correct: "Rot3"
+},
+{
+    question : "Which is not associated with computers?",
+    answers: ["Turing Machine", "Babbage Engine", "CPU Overdrive", "FPU Manifold"],
+    correct: "FPU Manifold"
+},
+{
+    question : "Which of the following is not a method for encoding text characters on a computer?",
+    answers: ["EBCDIC", "ASCII", "SIXBIT", "ANSI"],
+    correct: "ANSI"
+},
+{
+    question : "McAfee, the internet security company, became a subsidiary of which of these in 2011?",
+    answers: ["Dell", "Microsoft", "IBM", "Intel"],
+    correct: "Intel"
+},
+{
+    question : "What does ICMP stand for?",
+    answers: ["Internet Control Message Protocol", "Internal Conflict Management Program", "Internet Connection Modem Protocol", "Intranet Control Message Program"],
+    correct: "Internet Control Message Protocol"
+},
+{
+    question : "Algorithms are composed of control structures. Which of these is not a control structure?",
+    answers: ["Pseudocode", "Sequence", "Iteration", "Selection"],
+    correct: "Pseudocode"
+},
+{
+    question : "Which of the following is the lowest level of computer language?",
+    answers: ["Machine language", "PASCAL", "FORTRAN", "Assembler"],
+    correct: "Machine language"
+}];
+
+
+
+// function shuffle(array) {
+//     for (let i = array.length - 1; i > 0; i--) {
+//       let j = Math.floor(Math.random() * (i + 1));
+//       [array[i], array[j]] = [array[j], array[i]];
+//     }
+//   }
+
